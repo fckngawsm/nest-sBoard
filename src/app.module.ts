00c32b81c70f733b6894da88ff9766e50bf0e4fb
Kirill <forworkskirill@gmail.com>
2023-07-13 18:35:55 +0500

    create service + bacis request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,8 @@
 import { Module } from "@nestjs/common";
-@Module({})
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+})
 export class AppModule {}
