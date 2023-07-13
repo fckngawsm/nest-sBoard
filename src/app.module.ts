73fd99410180ec23c2900027e7a6d71b8d20d38e
Kirill <forworkskirill@gmail.com>
2023-07-13 19:50:35 +0500

    create env for prop and dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from './users/users.module';
+import { UsersModule } from "./users/users.module";
+import { ConfigModule } from "@nestjs/config";
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "kirill",
-      database: "sBoard",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [],
       synchronize: true,
     }),
