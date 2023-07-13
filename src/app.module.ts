7d1d720f0d19ca7bc7aeec20ac0347b93133d2ba
Kirill <forworkskirill@gmail.com>
2023-07-13 19:01:14 +0500

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "kirill",
+      database: "sBoard",
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
