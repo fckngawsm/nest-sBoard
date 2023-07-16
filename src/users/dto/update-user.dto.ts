826aa37acce7c0e27dff2524e197d28051e8edcb
Kirill <forworkskirill@gmail.com>
2023-07-16 21:55:28 +0500

    add avatar

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -4,4 +4,10 @@ export class UpdateUserDto {
   name: string;
   @ApiProperty({ example: "Marchenko", description: "Фамилия" })
   lastName: string;
+  @ApiProperty({
+    example:
+      "https://cdn.dribbble.com/users/2313212/screenshots/11256142/media/27b57b3ee2ac221dc8c616d02161d96b.jpg?resize=400x0",
+    description: "Аватар пользователя",
+  })
+  avatar: string;
 }
